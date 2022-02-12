241206ac48c10b80ea13708a72f78d0b9a19e60a
Tyson Franks <tjfranks02@hotmail.com>
2022-02-12 16:24:24 +1000

    Refactored from using history module to useNavigation hook in react-router-dom.

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 
 import App from './components/App';
-import history from './history';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <BrowserRouter history={history}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   rootElement
